ffba115001c1726b4aa6b9cf60170e694c926535
Viki Fernando <viki.fernando2@gmail.com>
2023-11-30 15:11:31 +0700

    algoritma level-0

---
+++ java/level-0/VariableExchange.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class VariableExchange {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void exchange1() {
+
+    }
+
+    public static void variableExchange2() {
+
+    }
+}
---
+++ python/level_0/odd_even.py
@@ -0,0 +1,11 @@
+input_number = int(input("Input a number : "))
+
+
+def odd_even():
+    if input_number % 2 == 0:
+        print("EVEN")
+    else:
+        print("ODD")
+
+
+odd_even()
