a6bb747f18cc25b5eefade7564508eb09b35898c
Viki Fernando <viki.fernando2@gmail.com>
2023-11-30 14:12:28 +0700

    first commit

---
+++ java/level-0/OddEven.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class OddEven {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        oddEven();
+    }
+
+    public static void oddEven() {
+        System.out.println("Input number : ");
+        int n = sc.nextInt();
+
+        if (n % 2 == 0) {
+            System.out.println("EVEN");
+        } else {
+            System.out.println("ODD");
+        }
+    }
+}
